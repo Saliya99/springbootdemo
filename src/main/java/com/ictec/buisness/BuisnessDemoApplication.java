0dd95d4abc6bcb2bfe2ef9fcf6f567a9eed522bc
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-06-07 07:08:04 +0530

    BusinessDemoApplication

--- src/main/java/com/ictec/buisness/BuisnessDemoApplication.java
+++ src/main/java/com/ictec/buisness/BuisnessDemoApplication.java
@@ -1,13 +1,25 @@
 package com.ictec.buisness;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BuisnessDemoApplication {
+    @Value("${type}")
+    String type;
 
     public static void main(String[] args) {
         SpringApplication.run(BuisnessDemoApplication.class, args);
     }
 
+    @Bean
+    public URLConfig createURLConfigOb(){
+        if(this.type.equals("local")){
+            return new URLConfig("http", "localhost", "8080");
+        }else{
+            return new URLConfig("https", "ictec.com");
+        }
+    }
 }
